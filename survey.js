9d199185e85545fccebd220ae6915d1ae0f55d45
unknown <VicSugi>
2021-07-20 18:01:29 -0300

    15

--- survey.js
+++ survey.js
@@ -11,7 +11,7 @@ var json = {
                     "type": "text",
                     "name": "nome",
                     "title": "Nome",
-                    "placeholder": "Digite seu nome",
+                    placeholder: "Digite seu nome",
                     "isRequired": true
                 },
                 {
@@ -26,7 +26,7 @@ var json = {
                     "name": "eficiencia",
                     "title": "Avalie a eficiência deste colaborador:",
                     "isRequired": true,
-                    "rateMin": 0,
+                    "rateMin": 1,
                     "rateMax": 10,
                 },
                 {
@@ -40,7 +40,7 @@ var json = {
                     "name": "eficacia",
                     "title": "Avalie a eficácia deste colaborador:",
                     "isRequired": true,
-                    "rateMin": 0,
+                    "rateMin": 1,
                     "rateMax": 10,
                 },
                 {
@@ -53,7 +53,7 @@ var json = {
                     "name": "postura",
                     "title": "Avalie a postura deste colaborador:",
                     "isRequired": true,
-                    "rateMin": 0,
+                    "rateMin": 1,
                     "rateMax": 10,
                 },
                 {
@@ -71,7 +71,7 @@ var json = {
                     "name": "eficiencia",
                     "title": "Avalie a eficiência deste colaborador:",
                     "isRequired": true,
-                    "rateMin": 0,
+                    "rateMin": 1,
                     "rateMax": 10,
                 },
                 {
@@ -85,7 +85,7 @@ var json = {
                     "name": "eficacia",
                     "title": "Avalie a eficácia deste colaborador:",
                     "isRequired": true,
-                    "rateMin": 0,
+                    "rateMin": 1,
                     "rateMax": 10,
                 },
                 {
@@ -98,7 +98,142 @@ var json = {
                     "name": "postura",
                     "title": "Avalie a postura deste colaborador:",
                     "isRequired": true,
-                    "rateMin": 0,
+                    "rateMin": 1,
+                    "rateMax": 10,
+                },
+                {
+                    "type": "comment",
+                    "name": "posturacomment",
+                    "title": "Algum comentário adicional?"
+                },
+            ]
+        },
+        {
+            "name": "page2",
+            "elements": [
+                {
+                    "type": "rating",
+                    "name": "eficiencia",
+                    "title": "Avalie a eficiência deste colaborador:",
+                    "isRequired": true,
+                    "rateMin": 1,
+                    "rateMax": 10,
+                },
+                {
+                    "type": "comment",
+                    "name": "eficienciacomment",
+                    "title": "Algum comentário adicional?",
+                    "placeholder":"está relacionado a eficiência do liderado em realizar entregas no prazo e dentro do orçamento estabelecido para a atividade."
+                },
+                {
+                    "type": "rating",
+                    "name": "eficacia",
+                    "title": "Avalie a eficácia deste colaborador:",
+                    "isRequired": true,
+                    "rateMin": 1,
+                    "rateMax": 10,
+                },
+                {
+                    "type": "comment",
+                    "name": "eficaciacomment",
+                    "title": "Algum comentário adicional?"
+                },
+                {
+                    "type": "rating",
+                    "name": "postura",
+                    "title": "Avalie a postura deste colaborador:",
+                    "isRequired": true,
+                    "rateMin": 1,
+                    "rateMax": 10,
+                },
+                {
+                    "type": "comment",
+                    "name": "posturacomment",
+                    "title": "Algum comentário adicional?"
+                },
+            ]
+        },
+        {
+            "name": "page2",
+            "elements": [
+                {
+                    "type": "rating",
+                    "name": "eficiencia",
+                    "title": "Avalie a eficiência deste colaborador:",
+                    "isRequired": true,
+                    "rateMin": 1,
+                    "rateMax": 10,
+                },
+                {
+                    "type": "comment",
+                    "name": "eficienciacomment",
+                    "title": "Algum comentário adicional?",
+                    "placeholder":"está relacionado a eficiência do liderado em realizar entregas no prazo e dentro do orçamento estabelecido para a atividade."
+                },
+                {
+                    "type": "rating",
+                    "name": "eficacia",
+                    "title": "Avalie a eficácia deste colaborador:",
+                    "isRequired": true,
+                    "rateMin": 1,
+                    "rateMax": 10,
+                },
+                {
+                    "type": "comment",
+                    "name": "eficaciacomment",
+                    "title": "Algum comentário adicional?"
+                },
+                {
+                    "type": "rating",
+                    "name": "postura",
+                    "title": "Avalie a postura deste colaborador:",
+                    "isRequired": true,
+                    "rateMin": 1,
+                    "rateMax": 10,
+                },
+                {
+                    "type": "comment",
+                    "name": "posturacomment",
+                    "title": "Algum comentário adicional?"
+                },
+            ]
+        },
+        {
+            "name": "page2",
+            "elements": [
+                {
+                    "type": "rating",
+                    "name": "eficiencia",
+                    "title": "Avalie a eficiência deste colaborador:",
+                    "isRequired": true,
+                    "rateMin": 1,
+                    "rateMax": 10,
+                },
+                {
+                    "type": "comment",
+                    "name": "eficienciacomment",
+                    "title": "Algum comentário adicional?",
+                    "placeholder":"está relacionado a eficiência do liderado em realizar entregas no prazo e dentro do orçamento estabelecido para a atividade."
+                },
+                {
+                    "type": "rating",
+                    "name": "eficacia",
+                    "title": "Avalie a eficácia deste colaborador:",
+                    "isRequired": true,
+                    "rateMin": 1,
+                    "rateMax": 10,
+                },
+                {
+                    "type": "comment",
+                    "name": "eficaciacomment",
+                    "title": "Algum comentário adicional?"
+                },
+                {
+                    "type": "rating",
+                    "name": "postura",
+                    "title": "Avalie a postura deste colaborador:",
+                    "isRequired": true,
+                    "rateMin": 1,
                     "rateMax": 10,
                 },
                 {
@@ -147,22 +282,3 @@ document
         var pdfHeight = survey.pdfHeight || 297;
         saveSurveyToPdf("RelatorioAVDITech.pdf", survey, pdfWidth, pdfHeight);
     };
-
-survey.data = {
-    'Quality': {
-        'affordable': '3',
-        'does what it claims': '4',
-        'better then others': '3',
-        'easy to use': '5'
-    },
-    'satisfaction': '4',
-    'recommend friends': '4',
-    'suggestions': '24/7 support would help a lot.',
-    'price to competitors': 'Not sure',
-    'price': 'correct',
-    'pricelimit': {
-        'mostamount': 450,
-        'leastamount': 200
-    },
-    'email': 'jon.snow@nightwatch.org'
-};
